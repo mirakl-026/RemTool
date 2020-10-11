400305ed9ab31f36e77416252693b7541c8e0d8b
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-10-11 16:15:52 +0300

    добавил контекст, пока без реализации интерейсов

---
+++ RemTool/Services/SqlSE/ApplicationContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using RemTool.Models;
+
+namespace RemTool.Services.SqlSE
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+
+        public DbSet<Tool> Tools { get; set; }
+
+        public DbSet<SparePart> SpareParts { get; set; }
+
+    }
+}
