b43684dd75d7694582b3ad0da54e4dc0559bbc04
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-10-24 18:48:55 +0300

    добавил модель "типа инструмента"

---
+++ RemTool/Models/ToolType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace RemTool.Models
+{
+    public class ToolType
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int MainType { get; set; }
+
+        public int SecondaryType { get; set; }
+
+        public List<string> Brands { get; set; }
+
+        public Dictionary<string, int> ServeCost { get; set; }        
+
+        public string ImgReference { get; set; }
+    }
+}
