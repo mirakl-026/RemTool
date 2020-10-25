a35cc6672e55465905ec1e0dcff031f365d36fe7
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-10-25 17:17:37 +0300

    удалил ненужные компоненты из модуля админки

--- RemTool/ClientApp/src/app/admin/admin.module.ts
+++ RemTool/ClientApp/src/app/admin/admin.module.ts
@@ -1,35 +1,33 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from "@angular/core"
 import { RouterModule } from '@angular/router';
-import { LoginPageComponent } from './login-page/login-page.component';
+
 import { AdminLayoutComponent } from './shared/admin-layout/admin-layout.component';
-import { AddPageComponent } from './add-page/add-page.component';
-import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
-import { EditPageComponent } from './edit-page/edit-page.component';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+
 
 @NgModule({
-    declarations: [
-        AdminLayoutComponent,
-        LoginPageComponent,
-        DashboardPageComponent,
-        AddPageComponent,
-        EditPageComponent
-    ],
-    imports: [
-        CommonModule,
-        RouterModule.forChild([
-            {
-                path: '', component: AdminLayoutComponent, children: [
-                    {path: '', redirectTo: '/admin/login', pathMatch: 'full'},
-                    {path: 'login', component: LoginPageComponent},
-                    {path: 'dashboard', component: DashboardPageComponent},
-                    {path: 'add', component: AddPageComponent},
-                    {path: 'tool/:id/edit', component: EditPageComponent},
-                ]
-            }
-        ])
-    ],
-    exports: [RouterModule],
+  declarations: [
+    AdminLayoutComponent,
+  ],
+  imports: [
+    FormsModule,
+    HttpClientModule,
+    CommonModule,
+    RouterModule.forChild([
+        {
+            path: '', component: AdminLayoutComponent, children: [
+
+            ]
+        }
+    ])
+  ],
+  exports: [RouterModule],
 })
 
 export class AdminModule {
