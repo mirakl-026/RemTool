2d46ab81d9862165179b3c8e02352f4215f8f29e
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-11-01 19:42:46 +0300

    реализовал контроллеры со стороны бэка

--- RemTool/Controllers/ClickCounterController.cs
+++ RemTool/Controllers/ClickCounterController.cs
@@ -5,11 +5,57 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
+using RemTool.Models;
+using RemTool.Infrastructure.Interfaces.Services;
+
 namespace RemTool.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/counters")]
     [ApiController]
     public class ClickCounterController : ControllerBase
     {
+        private readonly IClickCounterService db;
+
+        public ClickCounterController(IClickCounterService context)
+        {
+            db = context;
+        }
+
+        // Get{id} - get counter value by counter id
+        [HttpGet("{id}")]
+        public int Get(string id)
+        {
+            ClickCounter clickCounter = db.ReadClickCounter(id);
+            if (clickCounter != null)
+            {
+                return clickCounter.Count;
+            }
+            return 0;
+        }
+
+        // Put{id} - increase counter
+        [HttpPut("{counterid}/{tooltypeId}")]
+        public IActionResult Put(string counterId, string tooltypeId)
+        {
+            if (ModelState.IsValid)
+            {
+                ClickCounter clickCounter = db.ReadClickCounter(counterId);
+                if (clickCounter != null)
+                {
+                    db.IncreaseCounter(clickCounter.Id, tooltypeId);
+                }                
+                return Ok();
+            }
+            return BadRequest(ModelState);
+        }
+
+
+        // Delete{id} - reset counter by id
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            db.ResetCounter(id);
+            return Ok();
+        }
     }
 }
--- RemTool/Controllers/RtRequestController.cs
+++ RemTool/Controllers/RtRequestController.cs
@@ -5,11 +5,59 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
+using RemTool.Models;
+using RemTool.Infrastructure.Interfaces.Services;
+
 namespace RemTool.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/rtrequests")]
     [ApiController]
     public class RtRequestController : ControllerBase
     {
+        private readonly IRtRequestService db;
+
+        public RtRequestController(IRtRequestService context)
+        {
+            db = context;
+        }
+
+        // Get - get all requests
+        [HttpGet]
+        public IEnumerable<RtRequest> Get()
+        {
+            return db.ReadAllRtRequests();
+        }
+
+        // Get{id} - get request by id
+        [HttpGet("{id}")]
+        public RtRequest Get(string id)
+        {
+            RtRequest rtreq = db.ReadRtRequest(id);
+            return rtreq;
+        }
+
+        // Post - add request
+        [HttpPost]
+        public IActionResult Post(RtRequest newRtreq)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CreateRtRequest(newRtreq);
+                return Ok(newRtreq);
+            }
+            return BadRequest(ModelState);
+        }
+
+        // Delete{id} - delete request
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            RtRequest rtreq = db.ReadRtRequest(id);
+            if (rtreq != null)
+            {
+                db.DeleteRtRequest(id);
+            }
+            return Ok(rtreq);
+        }
     }
 }
