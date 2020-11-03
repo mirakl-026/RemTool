7839880973be4447cad37d9a6d6e3600e93473ec
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-11-03 19:57:55 +0300

    Добавил Nuget и опции аутентификации

--- RemTool/Infrastructure/Additional/AuthOptions.cs
+++ RemTool/Infrastructure/Additional/AuthOptions.cs
@@ -1,11 +1,25 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace RemTool.Infrastructure.Additional
 {
     public class AuthOptions
     {
+        public string Issuer { get; set; }
+
+        public string Audience { get; set; }
+
+        public string Secret { get; set; }
+
+        public int TokenLifeTime { get; set; }  // seconds
+
+        public SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Secret));
+        }
     }
 }
