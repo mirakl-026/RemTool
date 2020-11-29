eb19a31136700a3544899b28ebc27c302d87aa96
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-11-29 21:01:44 +0300

    Добавил контроллер

---
+++ RemTool/Controllers/SearchController.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using RemTool.Infrastructure.Interfaces.Services;
+
+namespace RemTool.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly IToolTypeSearchService _context;
+
+        public SearchController(IToolTypeSearchService context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<string> Find(string userInput)
+        {
+            return await _context.SearchAsync(userInput);
+        }
+    }
+}
