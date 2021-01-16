d27e3d2336d8b6e72cf784b3eb0c6f73b1645d43
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2021-01-16 16:53:31 +0300

    добавил модель "настройки почты"

---
+++ RemTool/Models/MailSettings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace RemTool.Models
+{
+    public class MailSettings
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        // флаг об отправке оповещений на почту админа
+        public bool SendMailToHQ { get; set; }
+
+        // почта админа для оповещений
+        public string HQ_mail { get; set; }
+
+        // флаг об отправке на почту запросящего
+        public bool SendMailToRequester { get; set; }
+
+        // сообщение по умолчанию в письме запросящему
+        public string DefaultMessageToRequester { get; set; }
+
+
+        // почта за счёт которой идёт отправка
+        public string senderMail { get; set; }
+
+        public string senderPass { get; set; }
+    }
+}
