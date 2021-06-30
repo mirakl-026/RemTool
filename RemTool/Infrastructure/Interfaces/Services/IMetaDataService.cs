868cf9ddea01768fc7527389222b236b45e8cb6b
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2021-06-30 18:16:35 +0300

    добавил модель метаданных (телефон), интерфейс сервиса и реализацию сервиса

---
+++ RemTool/Infrastructure/Interfaces/Services/IMetaDataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using RemTool.Models;
+
+namespace RemTool.Infrastructure.Interfaces.Services
+{
+    public interface IMetaDataService
+    {
+        #region CRUD_sync
+
+        public void CreateMetaData(MetaData md);
+
+        public MetaData ReadMetaData();
+
+        public void UpdateMetaData(MetaData newMd);
+
+        public void DeleteMetaData();
+
+        #endregion
+
+        public Task<MetaData> ReadMetaDataAsync();
+    }
+}
---
+++ RemTool/Models/MetaData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+// общие мета данные - типа номера телефона , ссылки на карты и т.д.
+namespace RemTool.Models
+{
+    public class MetaData
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        // флаг об отправке оповещений на почту админа
+        public string PhoneNumber { get; set; }
+    }
+}
