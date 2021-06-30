8f4e7b1ec8c23e7fbf0a8ab555cefabae8d713d4
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2021-06-30 18:42:46 +0300

    контроллер для меты

---
+++ RemTool/Controllers/MetaDataController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using RemTool.Models;
+using RemTool.Infrastructure.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace RemTool.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MetaDataController : ControllerBase
+    {
+        private IMetaDataService db;
+
+        public MetaDataController (IMetaDataService context)
+        {
+            db = context;
+        }
+
+        // GET api/MetaData/GetMetaData
+        [HttpGet("GetMetaData")]
+        public async Task<MetaData> GetMetaData()
+        {
+            return await db.ReadMetaDataAsync();
+        }
+
+
+        // GET api/MetaData/GetPhoneNumber
+        [HttpGet("GetPhoneNumber")]
+        public async Task<string> GetPhoneNumber()
+        {
+            MetaData md = await db.ReadMetaDataAsync();
+            return md.PhoneNumber;
+        }
+
+        // POST api/MetaData/SetPhoneNumber
+        [HttpPost("SetPhoneNumber")]
+        public void SetPhoneNumber(string number)
+        {
+            MetaData md = db.ReadMetaData();
+            md.PhoneNumber = number;
+            db.UpdateMetaData(md);
+        }
+    }
+}
